034ccb19fabc5a7d1945ed58cf20e8c596aeb116
NETLINK <julianmatz@netlink.ie>
2013-03-16 19:12:46 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,41 +9,8 @@ sidebars, comments, ect.
 */
 
 /************* INCLUDE NEEDED FILES ***************/
+require_once('library/includes.php')
 
-/*
-1. library/bones.php
-	- head cleanup (remove rsd, uri links, junk css, ect)
-	- enqueueing scripts & styles
-	- theme support functions
-	- custom menu output & fallbacks
-	- related post function
-	- page-navi function
-	- removing <p> from around images
-	- customizing the post excerpt
-	- custom google+ integration
-	- adding custom fields to user profiles
-*/
-require_once('library/bones.php'); // if you remove this, bones will break
-/*
-2. library/custom-post-type.php
-	- an example custom post type
-	- example custom taxonomy (like categories)
-	- example custom taxonomy (like tags)
-*/
-require_once('library/custom-post-type.php'); // you can disable this if you like
-/*
-3. library/admin.php
-	- removing some default WordPress dashboard widgets
-	- an example custom dashboard widget
-	- adding custom login css
-	- changing text in footer of admin
-*/
-// require_once('library/admin.php'); // this comes turned off by default
-/*
-4. library/translation/translation.php
-	- adding support for other languages
-*/
-// require_once('library/translation/translation.php'); // this comes turned off by default
 
 /************* THUMBNAIL SIZE OPTIONS *************/
 
