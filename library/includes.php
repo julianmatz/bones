019c9421ff91d5aed0a80ff77563483a884f2ea5
Julian Matz <julianmatz@netlink.ie>
2013-03-16 19:32:02 +0000

    Add includes.php file, move include files to includes directory.

--- library/includes.php
+++ library/includes.php
@@ -12,14 +12,14 @@
 	- custom google+ integration
 	- adding custom fields to user profiles
 */
-require_once('library/bones.php'); // if you remove this, bones will break
+require_once('includes/bones.php'); // if you remove this, bones will break
 /*
 2. library/custom-post-type.php
 	- an example custom post type
 	- example custom taxonomy (like categories)
 	- example custom taxonomy (like tags)
 */
-require_once('library/custom-post-type.php'); // you can disable this if you like
+require_once('includes/custom-post-type.php'); // you can disable this if you like
 /*
 3. library/admin.php
 	- removing some default WordPress dashboard widgets
@@ -27,7 +27,7 @@ require_once('library/custom-post-type.php'); // you can disable this if you lik
 	- adding custom login css
 	- changing text in footer of admin
 */
-// require_once('library/admin.php'); // this comes turned off by default
+// require_once('includes/admin.php'); // this comes turned off by default
 /*
 4. library/translation/translation.php
 	- adding support for other languages
